92a85abe66818a59831d0e7c42cf543d3f791d79
Prashant230799 <148067942+Prashant230799@users.noreply.github.com>
2023-11-18 10:58:25 +0530

    Update sum_of_digit.c
    change code addition to subtraction

--- sum_of_digit.c
+++ sum_of_digit.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int a,b,sum;
-    printf("Enter the value of a,b");
+    //int a,b,sum;
+    //printf("Enter the value of a,b");
+    int a,b,sub;
+    printf("Enter the value of a and b");
     scanf("%d %d",a,b);
-    sum=a+b;
-    printf("Sum is %d",sum);
+   // sum=a+b;
+    //printf("Sum is %d",sum);
+    sub=a-b;
+    printf("%d",sub);
     return 0;
 }
